60b1723060d7be82978be7418fabef646d0ccf66
fazte <gafarchuk@icloud.com>
2023-09-07 16:21:19 +0300

    asda

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,10 +1,23 @@
 let city = prompt('наименование города');
 let year = prompt('Какой год');
-let born = prompt('год когда был основан город');
-let pop = prompt('кол-во населения');
-
-let year2 = Number(year);
-let born2 = Number(born);
-
-alert ('Городу'+ ' '  + city + ' ' + 'исполнилось'+ ' ' + (year2 - born2) + ' ' +' лет с момента его образования. Население - '+ ' ' + pop + ' ' +' человек')
-alert ('https://github.com/fazte/sm2');
+if(year != 2023){
+	alert ('Подсказка, сейчас 2023 год, давай по новой')
+	location.reload();
+}else{
+	let born = prompt('год когда был основан город');
+	if(born >=2020){
+		alert ('Не верю, давай по новой')
+		location.reload();
+	}else{
+	let pop = prompt('кол-во населения');
+		if(isNaN(pop)){
+			alert('Надо число, давай по новой');
+			location.reload();
+		}else{
+			alert ('Городу'+ ' '  + city + ' ' + 'исполнилось'+ ' ' + (year - born) + ' ' +' лет с момента его образования. Население - '+ ' ' + pop + ' ' +' человек')
+			alert ('https://github.com/fazte/sm2');
+		}
+	
+	}
+	
+}
